70f6986f80576e07614c669300c27e66e0c35209
manhtieneaut <20200892@eaut.edu.vn>
2023-02-05 13:20:43 +0700

    bài thực hành 1

---
+++ src/HelloWorld.js
@@ -0,0 +1,20 @@
+import React from 'react';
+export default function HelloWorld() {
+  let name = "Cee"  
+
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+
+  const formatName = (user) => {
+    return user.firstName + ' ' + user.lastName;
+  };
+
+ return (
+ <div>
+    <div>Xin chào {name} và {formatName(user)}</div>
+    <div>Rất vui được gặp bạn</div>
+ </div>
+ );
+}
